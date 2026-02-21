7f9e79a5abbfdd4e5b3a8cb215b40477fa59b8ad
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-21 21:16:51 +0200

    feat:intial commit with setup environment

---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <section className="bg-black w-full h-screen p-5">
+      <div className="bg-white w-full h-full flex justify-center items-center rounded-xl">
+        <button 
+        className='
+        
+         rounded-xl
+         w-16 h-16
+         text-2xl
+         shadow-xl
+          bg-gray-50'>
+        1</button>
+      </div>
+    </section>
+  )
+}
+
+export default Main
