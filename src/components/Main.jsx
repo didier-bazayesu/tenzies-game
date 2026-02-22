f0a566cd85bad42e8b4194a4c035c66055f7300f
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-22 16:45:23 +0200

    feat: Roll buttons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 function Main() {
-    let[dice,setDice]= useState(generateAllNumber())
+    let[dice]= useState(generateAllNumber())
     function generateAllNumber (){
         let array = []
         for(let i=1 ; i<=10 ;i++ ) {
@@ -17,23 +17,25 @@ function Main() {
   return (
     <section className="bg-black w-full h-screen p-5">
 
-      <div className="bg-gray-200 w-full h-full flex justify-center items-center rounded-xl ">
+      <div className="bg-gray-200 w-full h-full flex justify-center items-center rounded-xl flex-col gap-y-5">
           <div className='grid grid-cols-5 grid-rows-2 gap-5'>
-        {dice.map((elem,index)=> 
-           <button 
-             key={index}
-            className='
-            hover:bg-amber-400
-            hover:cursor-pointer
-            rounded-xl
-            w-16 h-16
-            text-2xl
-            shadow-md
-            '>
-            {elem}</button>
-      )}
-       </div>
-      </div>
+                {dice.map((elem,index)=> 
+                    <button 
+                        key={index}
+                        className='
+                        hover:bg-amber-400
+                        hover:cursor-pointer
+                        rounded-xl
+                        w-16 h-16
+                        text-2xl
+                        shadow-md
+                        '>
+                    {elem}</button>
+                )}
+            </div>
+
+            <button className='bg-blue-700 text-center px-5 py-1.5 rounded-sm text-white cursor-pointer'>Roll</button>
+        </div>
     </section>
   )
 }
