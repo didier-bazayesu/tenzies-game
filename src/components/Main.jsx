a33d43f5c423ffe322650efb1f19bc9c559872a4
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-24 15:41:03 +0200

    feat: creating success message components once the Game over

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,28 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { nanoid } from 'nanoid';
 import Die from './Die';
-import Confetti from "react-confetti";
-import { useWindowSize } from "react-use";
-
-
-
+import SuccessMessage from './SuccessMessage';
 
 function Main() {
  
     let[dice,setDice]= useState(()=>generateAllNumber())
  
     //check won
-
     const firstValue = dice[0].value;
     const allSameValue = dice.every(die => die.value === firstValue);
     const gameWon = dice.every(die => die.isHeld) && allSameValue;
     const buttonRef = useRef();
-    const { width, height } = useWindowSize();
     const [showMessage, setShowMessage] = useState(false);
-
-
-   
-
 
     useEffect(() => {
         if (gameWon) {
@@ -30,7 +20,6 @@ function Main() {
             setShowMessage(true);
         }
     }, [gameWon])
-
 
     function generateAllNumber (){
         console.log("rendered")
@@ -46,13 +35,10 @@ function Main() {
         }else {
             setDice(generateAllNumber());
         }
-
     }
     
     function handleSingleButton(singleId){
-        
-        setDice(prev=> prev.map((elem,index)=> index == singleId ? {...elem , isHeld: !elem.isHeld} : elem))
-
+      setDice(prev=> prev.map((elem,index)=> index == singleId ? {...elem , isHeld: !elem.isHeld} : elem))
     } 
 
     function successMessage(){
@@ -60,47 +46,12 @@ function Main() {
             setDice(generateAllNumber());
     }
 
-  
-    
     let diceButtons = dice.map((elem, index )=> (<Die key={elem.id} handleSingleButton={()=>handleSingleButton(index)} value={elem.value} isHeld={elem.isHeld}/>))
 
-   
   return (
     <section className="bg-black w-full h-screen p-5">
 
-       {showMessage && (
-  <>
-    <Confetti
-      width={width}
-      height={height}
-      recycle={true}
-      numberOfPieces={400}
-    />
-
-    <div className="absolute inset-0 flex items-center justify-center bg-black/40 z-10">
-      <div className="bg-white px-8 py-6 rounded-xl shadow-xl text-center relative">
-        
-        <button
-          onClick={successMessage}
-          className="absolute top-2 right-3 text-gray-500 hover:text-black text-lg font-bold cursor-pointer"
-          
-        >
-          ×
-        </button>
-
-        <h2 className="text-2xl font-bold text-[#5035ff] mb-2">
-          🎉 Congratulations!
-        </h2>
-        <p className="text-gray-700">
-          You won the game. All dice match perfectly!
-        </p>
-      </div>
-    </div>
-  </>
-       )}
-
-
-
+          <SuccessMessage   successMessage={successMessage} showMessage={showMessage}/>
 
       <div className="bg-gray-200 w-full h-full flex justify-evenly items-center rounded-xl flex-col gap-y-5">
             <h1 className="text-xl font-bold">Tenzies</h1>
